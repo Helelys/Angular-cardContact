4720f334879905bd0a3bc61fd60d0054a1ea18f5
helelys <tallyslabanca20@gmail.com>
2025-03-20 21:26:09 -0300

    imagem inserida e redimensionada

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TextosComponent } from "../textos/textos.component";
+import { PictureComponent } from "../picture/picture.component";
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [TextosComponent],
+  imports: [TextosComponent, PictureComponent],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
