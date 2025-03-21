5e29d654eabe3c2787b8a8af812bf7a2dec5980d
helelys <tallyslabanca20@gmail.com>
2025-03-20 23:06:33 -0300

    finalizado

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { TextosComponent } from "../textos/textos.component";
 import { PictureComponent } from "../picture/picture.component";
+import { TitlesComponent } from "../titles/titles.component";
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [TextosComponent, PictureComponent],
+  imports: [TextosComponent, PictureComponent, TitlesComponent],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
